a03d34fd8d07dcfa9783b3254470cee2fab66b59
Tantalor93 <Obenky@gmail.com>
2018-07-18 18:30:12 +0200

    add solution of #845

---
+++ src/test/groovy/com/github/tantalor93/Solution845Spec.groovy
@@ -0,0 +1,26 @@
+package com.github.tantalor93
+
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class Solution845Spec extends Specification {
+
+    @Shared
+    def instance = new Solution845()
+
+    @Unroll
+    def "should find longest mountain for input"(int[] input, def expected) {
+        expect:
+        instance.longestMountain(input) == expected
+
+        where:
+        input                             | expected
+        [0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0] | 11
+        []                                | 0
+        [0, 1, 2, 3, 0, 1, 2, 1]          | 5
+        [5, 4, 3, 4, 5]                   | 0
+        null                              | 0
+    }
+}
