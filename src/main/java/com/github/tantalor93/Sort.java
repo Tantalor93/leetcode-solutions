83a8103effa43427a672d19de3377a2bfeb802c8
Tantalor93 <Obenky@gmail.com>
2018-08-19 12:48:56 +0200

    add quicksort

--- src/main/java/com/github/tantalor93/Sort.java
+++ src/main/java/com/github/tantalor93/Sort.java
@@ -6,9 +6,7 @@ public class Sort {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - i; j++) {
                 if (j + 1 < array.length && array[j] > array[j + 1]) {
-                    int tmp = array[j + 1];
-                    array[j + 1] = array[j];
-                    array[j] = tmp;
+                    flip(array, j, j+1);
                 }
             }
         }
@@ -16,17 +14,44 @@ public class Sort {
     }
 
     public static int[] insertSort(int[] array) {
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             int max = 0;
-            for(int j = 0; j < array.length-i;j++) {
-                if(array[max] < array[j]) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[max] < array[j]) {
                     max = j;
                 }
             }
-            int tmp = array[array.length -1 -i];
-            array[array.length-1-i] = array[max];
-            array[max] = tmp;
+            flip(array, array.length - 1 - i, max);
         }
         return array;
+    }
+
+    public static int[] quickSort(int[] array) {
+        doQuickSort(array, 0, array.length-1);
+        return array;
+    }
+
+    private static void doQuickSort(int[] array, int start, int end) {
+        if (!(start < end)) return;
+        int pivot = array[start];
+        int pivotIndex = start;
+        for (int i = start + 1; i <= end; i++) {
+            if(array[i] < pivot) {
+                flip(array, pivotIndex, pivotIndex+1);
+                if (pivotIndex + 1 != i)  {
+                    flip(array, pivotIndex, i);
+                }
+                pivotIndex++;
+            }
+        }
+        doQuickSort(array, start, pivotIndex - 1);
+        doQuickSort(array, pivotIndex + 1, end);
+
+    }
+
+    private static void flip(int[] array, int i, int j) {
+        int tmp = array[j];
+        array[j] = array[i];
+        array[i] = tmp;
     }
 }
--- src/test/groovy/com/github/tantalor93/SortTest.groovy
+++ src/test/groovy/com/github/tantalor93/SortTest.groovy
@@ -31,4 +31,18 @@ class SortTest extends SolutionSpecification {
 
         expected << [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [3, 3, 3, 3, 3], [], [1], [1, 2, 3, 4, 5]]
     }
+
+    @Unroll
+    def "should sort #array using quick sort"() {
+        when:
+        int[] result = Sort.quickSort(array as int[])
+
+        then:
+        result == expected as int[]
+
+        where:
+        array << [[1, 2, 3, 4, 5], [2, 1, 5, 4, 3], [3, 3, 3, 3, 3], [], [1], [5, 4, 3, 2, 1]]
+
+        expected << [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [3, 3, 3, 3, 3], [], [1], [1, 2, 3, 4, 5]]
+    }
 }
